d538fc18f309ecc613cbd6e0f17abcc352dca708
Mehran Khodaei <khodaei.dev@gmail.com>
2024-04-05 21:46:57 +0330

    create front page

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+require("dotenv").config();
+const path = require("path");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use("/css", express.static(path.join(__dirname, "public/style")));
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+app.use("/fonts", express.static(path.join(__dirname, "public/fonts")));
+
+app.use(express.urlencoded({ extended: false }));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", async (req, res) => {
+  res.render("index");
+});
+app.listen(process.env.Port, () => {
+  console.log(`Server Running on Port ${process.env.Port}`);
+});
