9e9edfef09fdf80a8be6a6d789ae51131871478c
Mehran Khodaei <khodaei.dev@gmail.com>
2024-04-07 00:36:02 +0330

    dev Create & Remove courses

--- controller/course.js
+++ controller/course.js
@@ -1,16 +1,34 @@
 const courseModel = require("./../model/course");
 
 exports.create = async (req, res) => {
-  //
+  try {
+    const { title } = req.body;
+    if (!title) {
+      return res.status(400).json({ message: "لطفا نام دوره را وارد کنید" });
+    }
+
+    await courseModel.create({ title });
+    return res.redirect("/courses");
+  } catch (err) {
+    return res.status(500).json("Ooops !!! Unknown Server Error :( ");
+  }
 };
 exports.getAll = async (req, res) => {
+  const courses = await courseModel.find({});
+
   res.render("index", {
-    courses: [],
+    courses,
     title: "Course Page",
   });
 };
 exports.remove = async (req, res) => {
-  //
+  try {
+    const { id } = req.params;
+    await courseModel.findOneAndDelete({ _id: id });
+    return res.redirect("/courses");
+  } catch (err) {
+    return res.status(500).json("Ooops !!! Unknown Server Error :( ");
+  }
 };
 exports.edit = async (req, res) => {
   //
