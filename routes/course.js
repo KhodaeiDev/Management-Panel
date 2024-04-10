9858ea4ac01f68f0c3b04928242c8bfd3a607576
Mehran Khodaei <khodaei.dev@gmail.com>
2024-04-10 21:08:21 +0330

    set isAdmin Middleware

--- routes/course.js
+++ routes/course.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const courseController = require("./../controller/course");
+const authMiddleware = require("./../middleware/auth");
+const isAdminMiddleware = require("./../middleware/isAdmin");
 
 const router = express.Router();
 
-router.route("/").get(courseController.getAll).post(courseController.create);
-router.route("/remove/:id").get(courseController.remove);
-router.route("/edit/:id").get(courseController.edit);
+router
+  .route("/")
+  .get(authMiddleware, isAdminMiddleware, courseController.getAll)
+  .post(authMiddleware, isAdminMiddleware, courseController.create);
+router
+  .route("/remove/:id")
+  .get(authMiddleware, isAdminMiddleware, courseController.remove);
+router
+  .route("/edit/:id")
+  .get(authMiddleware, isAdminMiddleware, courseController.edit);
 
 module.exports = router;
