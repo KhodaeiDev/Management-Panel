16bfa1685584ff90767333938f6f52cd50eda8e9
khodaeidev <khodaei.dev@gmail.com>
2024-06-15 13:52:44 +0330

    Edit body Parser and set Cors policy

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const path = require("path");
 const session = require("express-session");
 const flash = require("express-flash");
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const courseRouter = require("./routes/course");
 const loginRouter = require("./routes/login");
 const aboutRouter = require("./routes/about-me");
 const { erorHandller } = require("./middleware/errorHandller");
+const { setHeaders } = require("./middleware/setHeader");
 
 require("./configs/db");
 
@@ -17,12 +19,15 @@ const app = express();
 const baseUrl = process.env.BASE_URL;
 app.locals.baseUrl = baseUrl;
 
+// Cors Policy
+app.use(setHeaders);
+
 app.use("/css", express.static(path.join(__dirname, "public/style")));
 app.use("/js", express.static(path.join(__dirname, "public/js")));
 app.use("/images", express.static(path.join(__dirname, "public/images")));
 app.use("/fonts", express.static(path.join(__dirname, "public/fonts")));
 
-////////////
+// Sessions and Flash
 app.use(
   session({
     secret: "My secret key",
@@ -31,10 +36,9 @@ app.use(
   })
 );
 app.use(flash());
-//////////
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -48,8 +52,8 @@ app.use((req, res) => {
   return res.render("404");
 });
 
-// Error handller
-// app.use(erorHandller);
+// * Error handller
+app.use(erorHandller);
 
 app.listen(process.env.Port, () => {
   console.log(`Server Running on Port ${process.env.Port}`);
